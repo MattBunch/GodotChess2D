c182c02986a6fd345449215c56a595a42b41a40f
Matt Bunch <mattbunch@protonmail.com>
2021-09-21 22:54:01 +1200

    Refactor board creation to individual board tiles

--- Game.gd
+++ Game.gd
@@ -1,5 +1,5 @@
 extends Node2D
-class_name Board
+class_name Game
 
 const BOARD_SIZE = 480
 
@@ -10,13 +10,13 @@ var saved_tile
 
 func _ready():
 	connect_tiles()
+	initialize_pieces()
 
 func connect_tiles():
 	for tile in tiles:
 		tile.connect("clicked_tile",self,"clicked_tile")
 
 func clicked_tile(pos, tile_code):
-	
 	var tile = get_tile_at_pos(pos)
 	if saved_tile == tile:
 		print("same tile: ", saved_tile.get_tile_code())
@@ -40,18 +40,29 @@ func get_tile_at_pos(pos):
 func initialize_board():
 	var A = 65
 	var startNum = 8
+	var white = true
 	var output = []
 	var numCounter = startNum
 	var charCounter = A
 	for x in range(0, BOARD_SIZE, BOARD_SIZE / 8):
 		numCounter = startNum
+		white = !white
 		for y in range(0, BOARD_SIZE, BOARD_SIZE / 8):
 			var new_tile = load("res://Board/BoardTile.tscn").instance()
 			var tileCode = char(charCounter) + str(numCounter)
-			new_tile.init(tileCode)
+			new_tile.init(tileCode, white)
 			add_child(new_tile)
 			new_tile.set_tile_position(Vector2(x, y))
 			output.append([x, y])
 			numCounter -= 1
+			white = !white
 		charCounter += 1
 	return output
+
+func initialize_pieces():
+	for x in tiles:
+		print(x.get_tile_code())
+		# TODO: initialize pieces based on code tile starting positions
+
+func show_available_moves():
+	pass
